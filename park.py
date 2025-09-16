dd04a86a698ce76e442ba8ea928d52da766533eb
MigueM531 <MigueM531>
2025-09-16 12:59:21 -0500

    clase ParqueDiversiones con atracciones precargadas y encoladas

--- park.py
+++ park.py
@@ -10,3 +10,14 @@ class Atraccion:
     def __str__(self):
         return f"{self.nombre} ({self.capacidad}/turno) -> {self.visitantes}"
     
+class ParqueDiversiones:
+    def __init__(self):
+        self.atracciones = Queue() 
+        self._inicializar_atracciones()
+
+    def _inicializar_atracciones(self):
+        self.atracciones.enqueue(Atraccion("Montaña Rusa", 3))
+        self.atracciones.enqueue(Atraccion("Carros Chocones", 2))
+        self.atracciones.enqueue(Atraccion("Rueda de la Fortuna", 2))
+        self.atracciones.enqueue(Atraccion("Casa del Terror", 2))
+
