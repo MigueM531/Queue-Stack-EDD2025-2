2b1e6b47712e11bfa3c49125763b9db86b0dd27c
MigueM531 <MigueM531>
2025-09-16 12:56:30 -0500

    added class atraction

---
+++ park.py
@@ -0,0 +1,12 @@
+from class_queue import *
+from class_stack import *
+
+class Atraccion:
+    def __init__(self, nombre, capacidad):
+        self.nombre = nombre
+        self.capacidad = capacidad
+        self.visitantes = Stack()  # pila de visitantes
+
+    def __str__(self):
+        return f"{self.nombre} ({self.capacidad}/turno) -> {self.visitantes}"
+    
